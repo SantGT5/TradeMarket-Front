2bbfe4826cbcaddc7c102fed259b214f87efe78e
SantGT5 <gianspf@gmail.com>
2022-02-07 02:29:17 +0100

    Effects page aos

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -10,47 +10,49 @@ export const Login = () => {
   return (
     <form onSubmit={handleSubmit}>
       <div className="flex center login-img-container flex-direction">
-        <div>
+        <div data-aos="fade-right" data-aos-duration="2000">
           <img className="login-img" src={loginImg} alt="Login img" />
         </div>
-        <div className="login-container">
-          <h1 className="title">Login</h1>
-          <span className="login-sub-title">
-            See your growth and get consulting support!
-          </span>
-          <Input
-            placeholder="mail@ortex.com"
-            label="Email*"
-            classNameLabel="login-label-email"
-            classNameInput="login-input"
-            type="text"
-            onChange={handleChange}
-            value={status.email}
-            name="email"
-          />
-          <Input
-            placeholder="Min. 8 character"
-            label="Password*"
-            classNameLabel="login-label-pass"
-            classNameInput="login-input"
-            type={check ? "text" : "password"}
-            onChange={handleChange}
-            value={status.password}
-            name="password"
-          />
-          <div className="checkbox-container">
-            <input
-              className="checkbox"
-              type="checkbox"
-              name="show-pass"
-              id="check-id"
-              onChange={handleCheckbox}
+        <div data-aos="fade-up-left" data-aos-duration="2000">
+          <div className="login-container">
+            <h1 className="title">Login</h1>
+            <span className="login-sub-title">
+              See your growth and get consulting support!
+            </span>
+            <Input
+              placeholder="mail@ortex.com"
+              label="Email*"
+              classNameLabel="login-label-email"
+              classNameInput="login-input"
+              type="text"
+              onChange={handleChange}
+              value={status.email}
+              name="email"
             />
-            <label htmlFor="check-id">Show password</label>
+            <Input
+              placeholder="Min. 8 character"
+              label="Password*"
+              classNameLabel="login-label-pass"
+              classNameInput="login-input"
+              type={check ? "text" : "password"}
+              onChange={handleChange}
+              value={status.password}
+              name="password"
+            />
+            <div className="checkbox-container">
+              <input
+                className="checkbox"
+                type="checkbox"
+                name="show-pass"
+                id="check-id"
+                onChange={handleCheckbox}
+              />
+              <label htmlFor="check-id">Show password</label>
+            </div>
+            <button className="login-btn" type="submit">
+              LOGIN
+            </button>
           </div>
-          <button className="login-btn" type="submit">
-            LOGIN
-          </button>
         </div>
         <span className="logo-fixed-position">ORTEX</span>
       </div>
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -9,43 +9,45 @@ export const Profile = () => {
   return (
     <form onSubmit={handleSubmit}>
       <NavBar />
-      <div className="flex center">
-        <BiUser className="icon-profile" />
-      </div>
-      <div className="center-grid">
-        <Input
-          label="Current password*"
-          type="text"
-          placeholder="Your password"
-          classNameInput="profile-input"
-          classNameLabel="profile-label"
-          onChange={handleChange}
-          value={status.currentPassword}
-          name="currentPassword"
-        />
-        <Input
-          label="New password*"
-          type="text"
-          placeholder="New password"
-          classNameInput="profile-input"
-          classNameLabel="profile-label"
-          onChange={handleChange}
-          value={status.newPassword}
-          name="newPassword"
-        />
-        <Input
-          label="Confirm new password*"
-          type="text"
-          placeholder="Confirm new password"
-          classNameInput="profile-input"
-          classNameLabel="profile-label"
-          onChange={handleChange}
-          value={status.confirmPassword}
-          name="confirmPassword"
-        />
-        <button className="profile-btn" type="submit">
-          Set password
-        </button>
+      <div data-aos="zoom-out-down" data-aos-duration="2000">
+        <div className="flex center">
+          <BiUser className="icon-profile" />
+        </div>
+        <div className="center-grid">
+          <Input
+            label="Current password*"
+            type="text"
+            placeholder="Your password"
+            classNameInput="profile-input"
+            classNameLabel="profile-label"
+            onChange={handleChange}
+            value={status.currentPassword}
+            name="currentPassword"
+          />
+          <Input
+            label="New password*"
+            type="text"
+            placeholder="New password"
+            classNameInput="profile-input"
+            classNameLabel="profile-label"
+            onChange={handleChange}
+            value={status.newPassword}
+            name="newPassword"
+          />
+          <Input
+            label="Confirm new password*"
+            type="text"
+            placeholder="Confirm new password"
+            classNameInput="profile-input"
+            classNameLabel="profile-label"
+            onChange={handleChange}
+            value={status.confirmPassword}
+            name="confirmPassword"
+          />
+          <button className="profile-btn" type="submit">
+            Set password
+          </button>
+        </div>
       </div>
     </form>
   )
